2827ab3f3585f7ea3dcdbd9c942f78d868b95311
avijit <avijitd852@gmail.com>
2021-07-09 10:39:18 +0530

    connection

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv'); 
+// require('dotenv').config()
+const app = express();
+const server = require("http").Server(app);
+dotenv.config();
+
+const port = process.env.PORT || 5050;
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then((responce) => {
+    server.listen(port, () => {
+      console.log(`active on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
